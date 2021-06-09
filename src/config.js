732e764dfa80ff7186b26897064c0c40615a270b
Yooshin <yong.yooshin@gmail.com>
2021-06-09 23:29:24 +1000

    UPDATE backend endpoint for local development

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const { env } = process;
 const config = {
-  backend_endpoint: env.MOOOVIE_BACKEND || 'http://localhost:3030' 
+  backend_endpoint: env.MOOOVIE_BACKEND || 'http://localhost:3030/' 
 };
 
 export default config;
